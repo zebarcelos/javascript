d9c22aae4c75904be02718c563717cecd305220a
zebarcelos <joseribeiro.barcelos@gmail.com>
2023-03-03 13:43:06 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 document.write("Seu nome é " + nome.toUpperCase() + " porfavor troque urgentemente!")
+
+var a = 5+7
+var b = a+7
+var c = String(b + a)
+var d = a+c // Pelo NODE, temos o resultado de 3212 - que é a soma do String C (32) com A (12)
