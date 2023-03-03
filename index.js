673f5aff7b1cd6931df838ebd4c503133d25ec3a
zebarcelos <joseribeiro.barcelos@gmail.com>
2023-03-03 14:13:44 -0300

    par ou impar

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ var a = 5+7
 var b = a+7
 var c = String(b + a)
 var d = a+c // Pelo NODE, temos o resultado de 3212 - que é a soma do String C (32) com A (12)
+var numerorespondidopelocliente = 7
+var num = (numerorespondidopelocliente % 2 == 0) ? "Par" : "Impar" // Função para saber se numero é par ou impar
+
