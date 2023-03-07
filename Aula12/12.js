83ff7026fad380fa18269ecd0771283832ee9546
zebarcelos <joseribeiro.barcelos@gmail.com>
2023-03-07 01:52:16 -0300

    start of the rose's site

---
+++ Aula12/12.js
@@ -0,0 +1,10 @@
+var idade = 17;
+if (idade < 16) {
+    console.log('Não vota');
+} else if (idade >= 65 || idade < 18){
+    console.log('Voto Opcional')
+} else {
+    console.log('Voto obrigatório')
+}
+
+
